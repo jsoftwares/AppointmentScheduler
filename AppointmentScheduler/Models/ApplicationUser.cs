82cc1a1c917f920f2938f03076df1c8a178a837b
Jeffrey Onochie <jeff.ict@gmail.com>
2022-05-02 09:55:14 +0100

    Create Login & Register views, extend IdentityUser

---
+++ AppointmentScheduler/Models/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace AppointmentScheduler.Models
+{
+    [Required]
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
--- AppointmentScheduler/Program.cs
+++ AppointmentScheduler/Program.cs
@@ -1,4 +1,5 @@
 using AppointmentScheduler.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")) );
+builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
 
 var app = builder.Build();
 
